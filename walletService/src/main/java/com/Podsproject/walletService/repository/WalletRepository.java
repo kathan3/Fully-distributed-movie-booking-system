df980098bd04e7baa4eb6f61ac956e7b6a0c2c38
Kathan Mistry <71454756+kathan3@users.noreply.github.com>
2024-07-22 17:13:03 +0530

    Delete walletService directory

--- walletService/src/main/java/com/Podsproject/walletService/repository/WalletRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Podsproject.walletService.repository;
-
-import com.Podsproject.walletService.entity.Wallet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WalletRepository extends JpaRepository<Wallet, Integer> {
-    Wallet findByUserId(Integer userId);
-}
